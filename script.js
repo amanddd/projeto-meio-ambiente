8dcd9463800de309958ea75cfed86362aa06176b
amand <amandasantozdavi@hotmail.com>
2022-06-20 14:48:55 -0300

    projeto fase 1

---
+++ script.js
@@ -0,0 +1,9 @@
+function openModal (){
+    let modal = document.getElementById('dv-modal')
+    modal.style.display = 'Block'   
+}
+
+function closeModal(){
+    let modal = document.getElementById('dv-modal')
+    modal.style.display = 'none'
+}
